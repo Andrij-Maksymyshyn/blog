607ef5277c971ff60b0925d4533231988511a376
Andrij-Maksymyshyn <84930890+Andrij-Maksymyshyn@users.noreply.github.com>
2023-04-25 00:28:22 +0300

    fixing filtration-2

--- configs/regexp.enum.js
+++ configs/regexp.enum.js
@@ -6,5 +6,5 @@ module.exports = {
   OBJECT_ID: /^([0-9a-fA-F]{24})|([0-9a-fA-F]{24})$/,
   SORT_BY:
     /^asc\(title\)|desc\(title\)|asc\(_id\)|desc\(_id\)|asc\(viewsCount\)|desc\(viewsCount\)|asc\(date\)|desc\(date\)$/m,
-  DATE: /^\d{4}-\d{2}-\d{2}|\d{4}-\d{2}-\d{2} \d{2}:\d{2}$/
+  DATE: /^\d{4}-\d{2}-\d{2}|\d{4}-\d{2}-\d{2} \d{2}:\d{2}|^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)((-(\d{2}):(\d{2})|Z)?)$/
 };
